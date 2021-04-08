e34d0105d218d3f1b1e860cbb63b6de69be5aabd
Nikos Mouchtaris <mouchtaris@gmail.com>
2021-04-08 20:02:15 +0200

    Generate source html files

--- etc/env/dev.fish
+++ etc/env/dev.fish
@@ -1,2 +1,2 @@
 bind -M insert \cr 'tmux send-keys -t ct:Edit.2 Space'
-bind -M insert \cw 'tmux respawn-pane -t ct:Edit.2'
+bind -M insert \ct 'tmux send-keys -t ct:Edit.2 q ; tmux respawn-pane -t ct:Edit.2'
