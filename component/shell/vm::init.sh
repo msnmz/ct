b89e1863739172921daadc16e2014b4d7cea606e
Nikos Mouchtaris <mouchtaris@gmail.com>
2021-04-11 20:04:12 +0200

    Add ubuntu/arch vm:init.sh

---
+++ component/shell/vm::init.sh
@@ -0,0 +1,171 @@
+# vim: et ts=2 sw=2 ft=bash
+
+COMMON_PACKAGES=(
+  ccache
+  gcc
+  git
+  htop
+  make
+)
+DEBIAN_APT_NEEDS=(
+  apt-utils
+  dialog
+)
+PACMAN_AUR_NEEDS=(
+  base-devel
+  pacman-contrib
+)
+PACMAN_MIRROR_UTIL=rate-arch-mirrors  # AUR
+UBUNTU_MIRROR_UTIL=apt-smart          # pip3
+
+git:init() {
+  source="$1"; shift;
+  target="$1"; shift;
+
+	if test -d $target
+	then
+    git -C $target remote update --prune
+  else
+    git clone --depth 1 $source $target
+  fi
+}
+
+gh:init() {
+  name="$1"; shift;
+  target="$1"; shift;
+
+  source="https://github.com/$name.git"
+  git:init "$source" "$target"
+}
+
+config::apt:sources() {
+  echo "* Installing ubuntu apt sources"
+  sudo tee /etc/apt/sources.list >/dev/null <<-'EOS'
+    deb http://mirror.eu.kamatera.com/ubuntu focal main restricted
+    deb http://mirror.eu.kamatera.com/ubuntu focal-updates main restricted
+    deb http://mirror.eu.kamatera.com/ubuntu focal universe
+    deb http://mirror.eu.kamatera.com/ubuntu focal-updates universe
+    deb http://mirror.eu.kamatera.com/ubuntu focal multiverse
+    deb http://mirror.eu.kamatera.com/ubuntu focal-updates multiverse
+    deb http://mirror.eu.kamatera.com/ubuntu focal-backports main restricted universe multiverse
+    deb http://security.ubuntu.com/ubuntu focal-security main restricted
+    deb http://security.ubuntu.com/ubuntu focal-security universe
+    deb http://security.ubuntu.com/ubuntu focal-security multiverse
+EOS
+}
+
+config::pacman:mirrorlist() {
+  echo "* Installing arch pacman mirrorlist"
+  sudo tee /etc/pacman.d/mirrorlist >/dev/null <<-'EOS'
+    Server = https://archlinux.koyanet.lv/archlinux/$repo/os/$arch
+    Server = http://mirror.puzzle.ch/archlinux/$repo/os/$arch
+    Server = http://mirror.datacenter.by/pub/archlinux/$repo/os/$arch
+    Server = https://archlinux.uk.mirror.allworldit.com/archlinux/$repo/os/$arch
+    Server = http://mirror.easylee.nl/archlinux/$repo/os/$arch
+EOS
+}
+
+pacman:install() {
+  sudo pacman -Syu --noconfirm --needed "$@"
+}
+
+apt:install() {
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get update -y
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+  sudo env DEBIAN_FRONTEND=noninteractive apt-get install "$@" -y
+}
+
+aur:install() {
+  local name="$1"; shift;
+
+  curl https://aur.archlinux.org/cgit/aur.git/snapshot/$name.tar.gz >$name.tar.gz
+
+  tar xvf $name.tar.gz
+  cd $name
+
+  makepkg -sic --noconfirm
+}
+
+linux.distro() {
+  node_name="$(uname --nodename)"
+  case "$node_name" in
+    ubuntu-*)
+      echo "Ubuntu"
+      ;;
+    arch.*)
+      echo "Arch"
+      ;;
+    *)
+      printf 'Unknown node_name: %s\n' "$node_name" 1>&2
+      exit 1
+  esac
+}
+
+CDI_install_base_devel() {
+  case "$( linux.distro )" in
+    (Ubuntu)
+      apt:install "${COMMON_PACKAGES[@]}" "${DEBIAN_APT_NEEDS[@]}"
+      ;;
+    (Arch)
+      pacman:install "${COMMON_PACKAGES[@]}" "${PACMAN_AUR_NEEDS}"
+      ;;
+  esac
+}
+
+CDI_install_rbenv_build() {
+  target="$(rbenv root)"/plugins
+
+  mkdir -p "$target"
+  gh:init "rbenv/ruby-build" "$target/ruby-build"
+}
+
+CDI::user_paths:add() {
+  extra_path="$1"; shift;
+  echo 'export PATH="'"$extra_path"':$PATH"' >> ~/.user_paths
+}
+
+CDI_install_rbenv() {
+  gh:init "rbenv/rbenv" "$HOME/.rbenv"
+  cd ~/.rbenv && src/configure && make -C src
+  CDI::user_paths:add "$HOME/.rbenv/bin"
+  source "$HOME/.user_paths"
+  eval "$(rbenv init -)"
+  echo 'eval "$(rbenv init -)"' \
+    | tee -a "$HOME/.bash_profile" \
+    | tee -a "$HOME/.bashrc" \
+    >/dev/null
+
+  CDI_install_rbenv_build
+  curl -fsSL "https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-doctor" | bash
+}
+
+CDI_install_homebrew() {
+  curl -fsSL "https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh" | bash
+}
+
+CDI_install_ccache_to_user_paths() {
+  case "$( linux.distro )" in
+    Ubuntu)
+      CDI::user_paths:add "/lib/ccache"
+      ;;
+    Arch)
+      CDI::user_paths:add "/lib/ccache/bin"
+      ;;
+  esac
+}
+
+CDI_install_base_devel
+CDI_install_rbenv
+CDI_install_ccache_to_user_paths
+
+# Diagnose
+if false
+then
+  source "$HOME/.user_paths"
+  eval "$(rbenv init -)"
+
+  which gcc
+  which ccache
+
+  rbenv install 3.0.1
+fi
